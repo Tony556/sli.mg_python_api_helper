aaa3bb020795c9fdc00e88aad83f932114ec2353
Tony Sesek <leorijn223@gmail.com>
2016-06-04 23:13:50 -0600

    Added support for using just client ID.

--- README.md
+++ README.md
@@ -12,7 +12,20 @@ To begin using it, you must import it using `from slimghelper import slimgHelper
 
 Then initialize it, you will need your Client ID and Client Secret for this next step. You can get those by reading through [this simple tutorial - https://sli.mg/public/api#getstarted.](https://sli.mg/public/api#getstarted)
 
-Now, using any variable name you choose, call the class! For these next few examples, I will use `helper` as our name. Example: `helper = slimgHelper('<YOUR_CLIENT_ID>', '<YOUR_CLIENT_SECRET>')`
+***If you do not wish to use a Client Secret, don't supply it, and use secret keys when updating and deleting content.***
+
+Now, using any variable name you choose, call the class! For these next few examples, I will use `helper` as our name.
+
+Examples:
+
+  - With client secret:
+
+    - `helper = slimgHelper('<YOUR_CLIENT_ID>', '<YOUR_CLIENT_SECRET>')`
+
+  - Without client secret:
+
+    - `helper = slimgHelper('<YOUR_CLIENT_ID>')`
+
 
 Next, run `helper.getAccessRefreshTokens()` If it returns True, you have access to every function in the script, and API (as of June 4, 2016)!
 
@@ -20,7 +33,7 @@ Here's a nice list of Functions:
 
   - `getAccessRefreshTokens(returnTokens=False)`
 
-    - You need to run this in order to run the rest of the methods, as it aquires the needed Access Token. ***This reruns itself every 23 hours!***
+    - You need to run this in order to run the rest of the methods, as it aquires the needed Access Token. ***If the script runs continuously, you will need to rerun this every 24 hours to get new tokens (if you use a Client Secret)!***
 
     - Other Possible Arguments: returnTokens (false by default)
 
@@ -52,9 +65,9 @@ Here's a nice list of Functions:
 
       - 'shared' is whether it's public or not
 
-      - 'album_key' is the key for the album you want it to go into (if supplied)
+      - 'albumKey' is the key for the album you want it to go into (if supplied)
 
-      - 'album_secret' is not working yet.
+      - 'albumSecret' is used if you don't supply an Access Token/you don't own the album.
 
       - Extra info:
 
@@ -65,14 +78,18 @@ Here's a nice list of Functions:
         - waitForUploadCompletionForDataReturn will wait until the image is done uploading (or fails), and will return it's data.
 
 
-  - `updateMedia(` same as createMedia except without `type, data, size, and waitForUploadCompletionForDataReturn)`
+  - `updateMedia(` same as createMedia except without `type, data, size, and waitForUploadCompletionForDataReturn, mediaSecret='')`
 
     - Updates image based on given arguments
 
+    - `mediaSecret` is used if you don't supply an Access Token/you don't own the media.
 
-  - `deleteMedia(mediaKey)`
+
+  - `deleteMedia(mediaKey, mediaSecret='')`
 
     - Deletes image based on media key
+
+    - `mediaSecret` is used if you don't supply an Access Token/you don't own the media.
 
 
   - `getAlbumInfo(albumKey)`
@@ -88,17 +105,21 @@ Here's a nice list of Functions:
 
       - `mediaKeys` are photos you want to include in the album
 
-      - `mediaSecrets` does not work yet.
+      - `mediaSecrets` is used if you don't supply an Access Token/you don't own the media.
 
 
-- `updateAlbum(albumKey, same as createAlbum)`
+- `updateAlbum(albumKey, same as createAlbum, albumSecret='')`
 
     - Updates album based on arguments
 
+    - `albumSecret` is used if you don't supply an Access Token/you don't own the
 
-- `deleteAlbum(albumKey)`
+
+- `deleteAlbum(albumKey, albumSecret='')`
 
   - Deletes album based on album key
+
+  - `albumSecret` is used if you don't supply an Access Token/you don't own the album.
 
 
 - `getAlbumMedia(albumKey)`
--- slimghelper.py
+++ slimghelper.py
@@ -6,30 +6,46 @@ class slimgHelper(object):
 
     def __init__(self, client_id='', client_secret=''):
         super(slimgHelper, self).__init__()
+        #Initializing properties
+
+        # ID and Secret are found on the account page
         self.client_id = client_id
         self.client_secret = client_secret
+
+        #To be filled in by getAccessRefreshTokens
         self.access_token = ''
         self.refresh_token = ''
-        self.expires_in = 85800
-        self.payload = {}
-        self.isAnon = True
 
+        #Unused?
+        self.expires_in = 85800
+
+        #Default payload for 'anon' logins
+        self.payload = {'client_id': client_id}
+
+        #Header for 'anon' logins
+        if (client_secret == ''):
+            self.headers = {'Authorization': 'Client-ID '+client_id}
+        else:
+            self.headers = {}
+
+    #Used in getting the dict from the JSON data
     def JsonAsDict(self, requestInstance):
         return json.loads(requestInstance.text)['data']
 
+    #Used when using the Client ID and Access Token
     def getAccessRefreshTokens(self, returnTokens=False):
         url = 'https://api.sli.mg/token'
         payload = {'grant_type': 'client_credentials', 'client_id': self.client_id, 'client_secret': self.client_secret}
         response = requests.post(url, data=payload)
         json_dict = json.loads(response.text)
+        print json_dict
         self.access_token = json_dict['access_token']
         self.refresh_token = json_dict['refresh_token']
+
         # Subtracting 10 mins from expires_in lowers the risk of the user trying to do something with a bad token.
+        # Still unused.
         self.expires_in = (json_dict['expires_in'] - 600)
-
         self.payload = {'client_id': self.client_id, 'access_token': self.access_token}
-
-        threading.Timer(self.expires_in, self.GetAccessRefreshTokens()).start()
 
         if (returnTokens):
             return {'access_token': self.access_token, 'refresh_token': self.refresh_token}
@@ -38,17 +54,17 @@ class slimgHelper(object):
 
     def getUserAlbums(self, username):
         url = 'https://api.sli.mg/account/{}/albums/'.format(username)
-        return self.JsonAsDict(requests.get(url, params=self.payload))
+        return self.JsonAsDict(requests.get(url, params=self.payload, headers=self.headers))
 
     def getUserMedia(self, username, page=1):
         url = 'https://api.sli.mg/account/{}/media/{}'.format(username, page)
-        return self.JsonAsDict(requests.get(url, params=self.payload))['media']
+        return self.JsonAsDict(requests.get(url, params=self.payload, headers=self.headers))['media']
 
     # ---Endpoint section seperation--- #
 
     def getMediaInfo(self, mediaKey):
         url = 'https://api.sli.mg/media/{}'.format(mediaKey)
-        return self.JsonAsDict(requests.get(url, params=self.payload))
+        return self.JsonAsDict(requests.get(url, params=self.payload, headers=self.headers))
 
     def waitingForUpload(self, mediaKey):
         currentInfo = self.getMediaInfo(mediaKey)
@@ -87,16 +103,16 @@ class slimgHelper(object):
         # https://stackoverflow.com/questions/12118695/efficient-way-to-remove-keys-with-empty-values-from-a-dict
         payload = dict((k, v) for k, v in payload.iteritems() if v)
 
-        json_data = self.JsonAsDict(requests.post(url, data=self.payload))
+        json_data = self.JsonAsDict(requests.post(url, data=self.payload, headers=self.headers))
         if (waitForUploadCompletionForDataReturn == False):
             return json_data
         elif (waitForUploadCompletionForDataReturn):
             return self.waitingForUpload(json_data['media_key'])
 
     def updateMedia(self, mediaKey, title=None, description=None, shared=None, albumKey=None,
-                    albumSecret=None, tags=None):
+                    albumSecret='', tags=None, mediaSecret=''):
 
-        url = 'https://api.sli.mg/media/{}'.format(mediaKey)
+        url = 'https://api.sli.mg/media/{}/{}'.format(mediaKey, mediaSecret)
 
         payload = {}
 
@@ -113,18 +129,18 @@ class slimgHelper(object):
         # https://stackoverflow.com/questions/12118695/efficient-way-to-remove-keys-with-empty-values-from-a-dict
         payload = dict((k, v) for k, v in payload.iteritems() if v)
 
-        return self.JsonAsDict(requests.post(url, data=self.payload))
+        return self.JsonAsDict(requests.post(url, data=self.payload, headers=self.headers))
 
-    def deleteMedia(self, mediaKey):
-        url = 'https://api.sli.mg/media/{}/'.format(mediaKey)
+    def deleteMedia(self, mediaKey, mediaSecret = ''):
+        url = 'https://api.sli.mg/media/{}/{}'.format(mediaKey, mediaSecret)
 
-        return self.JsonAsDict(requests.delete(url, params=self.payload))
+        return self.JsonAsDict(requests.delete(url, params=self.payload, headers=self.headers))
 
     # ---Endpoint section seperation--- #
 
     def getAlbumInfo(self, albumKey):
         url = 'https://api.sli.mg/album/{}'.format(albumKey)
-        return self.JsonAsDict(requests.get(url, params=self.payload))
+        return self.JsonAsDict(requests.get(url, params=self.payload, headers=self.headers))
 
     def createAlbum(self, description=None, shared=None, mediaKeys=None, mediaSecrets=None, tags=None):
         url = 'https://api.sli.mg/album'
@@ -149,13 +165,10 @@ class slimgHelper(object):
         # https://stackoverflow.com/questions/12118695/efficient-way-to-remove-keys-with-empty-values-from-a-dict
         payload = dict((k, v) for k, v in payload.iteritems() if v)
 
-        return self.JsonAsDict(requests.post(url, data=self.payload))
+        return self.JsonAsDict(requests.post(url, data=self.payload, headers=self.headers))
 
-    def updateAlbum(self, albumKey, description=None, shared=None, mediaKeys=None, mediaSecrets=None, tags=None, albumSecret=None):
-        if(self.isAnon):
-            url = 'https://api.sli.mg/album/{}/{}'.format(albumKey, albumSecret)
-        else:
-            url = 'https://api.sli.mg/album/{}'.format(albumKey)
+    def updateAlbum(self, albumKey, description=None, shared=None, mediaKeys=None, mediaSecrets=None, tags=None, albumSecret=''):
+        url = 'https://api.sli.mg/album/{}/{}'.format(albumKey, albumSecret)
 
         payload = {}
 
@@ -179,27 +192,24 @@ class slimgHelper(object):
         print payload
         print url
 
-        return self.JsonAsDict(requests.post(url, data=payload))
+        return self.JsonAsDict(requests.post(url, data=payload, headers=self.headers))
 
-    def deleteAlbum(self, albumKey, albumSecret=None):
-        if (self.isAnon):
-            url = 'https://api.sli.mg/album/{}/{}'.format(albumKey, albumSecret)
-        else:
-            url = 'https://api.sli.mg/album/{}'.format(albumKey)
+    def deleteAlbum(self, albumKey, albumSecret=''):
+        url = 'https://api.sli.mg/album/{}/{}'.format(albumKey, albumSecret)
 
-        return self.JsonAsDict(requests.delete(url, params=self.payload))
+        return self.JsonAsDict(requests.delete(url, params=self.payload, headers=self.headers))
 
     def getAlbumMedia(self, albumKey):
         url = 'https://api.sli.mg/album/{}/media'.format(albumKey)
-        return self.JsonAsDict(requests.get(url, params=self.payload))['media']
+        return self.JsonAsDict(requests.get(url, params=self.payload, headers=self.headers))['media']
 
     # ---Endpoint section seperation--- #
 
     def browsePublic(self, page=1):
         url = 'https://api.sli.mg/browse/{}'.format(page)
         print url
-        return self.JsonAsDict(requests.get(url, params=self.payload))
+        return self.JsonAsDict(requests.get(url, params=self.payload, headers=self.headers))
 
     def browsePublicByTag(self, tag, type='sfw', page=1):
         url = 'https://api.sli.mg/browse/{}/{}/{}'.format(tag, type, page)
-        return self.JsonAsDict(requests.get(url, params=self.payload))
+        return self.JsonAsDict(requests.get(url, params=self.payload, headers=self.headers))
